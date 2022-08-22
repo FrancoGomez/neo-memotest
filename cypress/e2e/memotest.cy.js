91e478894322fd3b39998df6c60edc8602bc548e
FrancoGomez <mgomez21699@gmail.com>
2022-08-22 01:32:44 -0300

    Completa las ultimos dos tests propuestos en clase

--- cypress/e2e/memotest.cy.js
+++ cypress/e2e/memotest.cy.js
@@ -1,4 +1,6 @@
 context("Memotest", () => {
+  const NUMERO_DE_CUADROS = 16;
+
   describe("Juega al memotest", () => {
     it("Se asegura de que haya un tablero con cartas", () => {
       cy.visit("/");
@@ -27,10 +29,47 @@ context("Memotest", () => {
   });
 
   describe("Resuelve el juego", () => {
-    let mapaDePares, listadoDePares;
+    let mapaDePares, listaDePares;
 
-    it("Elige una combinacion erronea", () => {});
+    it("Elige una combinacion erronea", () => {
+      cy.window()
+        .then(async (win) => {
+          const pares = {};
+          let index = 0;
 
-    it("Elige todas las combinaciones correctas", () => {});
+          for await (let color of win.coloresRepetidosParaCypress) {
+            if (pares[color]) {
+              pares[color].push(`#carta-${index}`);
+            } else {
+              pares[color] = [`#carta-${index}`];
+            }
+
+            index++;
+          }
+
+          mapaDePares = pares;
+        })
+        .then(() => {
+          listaDePares = Object.values(mapaDePares);
+        })
+        .then(() => {
+          cy.get(listaDePares[0][0]).click();
+          cy.get(listaDePares[1][0]).click();
+        });
+    });
+
+    it("Elige todas las combinaciones correctas", () => {
+      const NUMERO_DE_PARES = NUMERO_DE_CUADROS / 2;
+      cy.get(".carta").should("have.length", NUMERO_DE_CUADROS);
+
+      listaDePares.forEach((par) => {
+        cy.get(par[0]).click();
+        cy.get(par[1]).click();
+      });
+
+      // El "1" equivale al primer intento erroneo.
+      // Si bien el texto de la alerta es mas extenso, con esto compruebo que el juego finalizo.
+      cy.get("#alerta").contains(`Ganaste! Intentos: ${NUMERO_DE_PARES + 1}`);
+    });
   });
 });
