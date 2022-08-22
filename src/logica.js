e3c56c2a0ef7beee454211ac9182b4b84b4d1cf8
FrancoGomez <mgomez21699@gmail.com>
2022-08-21 21:21:47 -0300

    modifica como la funcion mezclarCartas funciona, para que el nombre sea mas acorde a lo que esta hace. Ademas determina en una etapa mas tempara si la segunda carta clickeada es igual a la primera clickeada, para evitar evaluaciones innecesarias.

--- src/logica.js
+++ src/logica.js
@@ -4,14 +4,14 @@ const $contador = document.querySelector("#contador");
 const $comenzar = document.querySelector("#comenzar");
 
 let coloresCartas = [
-    "#7308a5",
-    "#0000ff",
-    "#ff7f00",
-    "#007900",
-    "#ffff00",
-    "#00ff00",
-    "#ba00ff",
-    "#feb300",
+  "#7308a5",
+  "#0000ff",
+  "#ff7f00",
+  "#007900",
+  "#ffff00",
+  "#00ff00",
+  "#ba00ff",
+  "#feb300",
 ];
 let coloresRepetidos;
 let $cartaGuardada = "";
@@ -20,145 +20,149 @@ let pares, intentos, tiempoJugando, intervalo;
 $comenzar.onclick = () => iniciarJuego();
 
 const iniciarJuego = () => {
-    reiniciarEstado();
-    manejarRonda();
+  reiniciarEstado();
+  manejarRonda();
 };
 
 const reiniciarEstado = () => {
-    borrarCartas();
-    actualizarAlerta('Presiona "Comenzar" para jugar');
-    clearInterval(intervalo);
-    pares = coloresCartas.length;
-    intentos = 0;
-    tiempoJugando = 0;
-    intervalo = "";
+  borrarCartas();
+  actualizarAlerta('Presiona "Comenzar" para jugar');
+  clearInterval(intervalo);
+  pares = coloresCartas.length;
+  intentos = 0;
+  tiempoJugando = 0;
+  intervalo = "";
 };
 
 const manejarRonda = () => {
-    mezclarColores();
-    generarCartas(Number(coloresRepetidos.length));
-    iniciarTemporizador();
-    desbloquearInput();
+  coloresRepetidos = [...devolverColoresMezcaldos(coloresCartas)];
+  generarCartas(Number(coloresRepetidos.length));
+  iniciarTemporizador();
+  desbloquearInput();
 };
 
 const borrarCartas = () => {
-    const $cartas = document.querySelectorAll("#tablero .carta");
-    if ($cartas.length !== 0) {
-        $cartas.forEach((carta) => {
-            carta.remove();
-        });
-    }
+  const $cartas = document.querySelectorAll("#tablero .carta");
+  if ($cartas.length !== 0) {
+    $cartas.forEach((carta) => {
+      carta.remove();
+    });
+  }
 };
 
 const generarCartas = (cantidad) => {
-    for (let i = 0; i < cantidad; i++) {
-        const $carta = crearCarta(`carta-${i}`, i);
-        $tablero.appendChild($carta);
-    }
+  for (let i = 0; i < cantidad; i++) {
+    const $carta = crearCarta(`carta-${i}`, i);
+    $tablero.appendChild($carta);
+  }
 };
 
 const iniciarTemporizador = () => {
-    intervalo = setInterval(() => {
-        tiempoJugando++;
-        actualizarAlerta(`Tiempo: ${tiempoJugando} segundos`);
-    }, 1000);
+  intervalo = setInterval(() => {
+    tiempoJugando++;
+    actualizarAlerta(`Tiempo: ${tiempoJugando} segundos`);
+  }, 1000);
 };
 
 const bloquearInput = () => {
-    $tablero.onclick = () => {};
+  $tablero.onclick = () => {};
 };
 
 const desbloquearInput = () => {
-    $tablero.onclick = (e) => manejarClick(e);
+  $tablero.onclick = (e) => manejarClick(e);
 };
 
 const actualizarAlerta = (texto) => {
-    $alerta.textContent = texto;
+  $alerta.textContent = texto;
 };
 
-const mezclarColores = () => {
-    coloresRepetidos = coloresCartas.concat(coloresCartas);
-    const arrayAuxiliar = [];
+const devolverColoresMezcaldos = (colores) => {
+  coloresRepetidos = colores.concat(colores);
+  const arrayAuxiliar = [];
 
-    while (coloresRepetidos.length !== 0) {
-        const numeroRandom = obtenerNumeroRandom(coloresRepetidos.length);
-        const colorRandom = coloresRepetidos.splice(numeroRandom, 1);
-        arrayAuxiliar.push(colorRandom[0]);
-    }
+  while (coloresRepetidos.length !== 0) {
+    const numeroRandom = obtenerNumeroRandom(coloresRepetidos.length);
+    const colorRandom = coloresRepetidos.splice(numeroRandom, 1);
+    arrayAuxiliar.push(colorRandom[0]);
+  }
 
-    coloresRepetidos = [...arrayAuxiliar];
+  return arrayAuxiliar;
 };
 
 const obtenerNumeroRandom = (maximo) => {
-    return Math.floor(Math.random() * maximo);
+  return Math.floor(Math.random() * maximo);
 };
 
 const manejarClick = (e) => {
-    const $cartaClickeada = e.target;
+  const $cartaClickeada = e.target;
 
-    if ($cartaClickeada.style.backgroundColor === "") return;
+  if (
+    $cartaClickeada.style.backgroundColor === "" ||
+    $cartaGuardada.id === $cartaClickeada.id
+  )
+    return;
 
-    revelarColorCarta($cartaClickeada, obtenerColorCarta($cartaClickeada.id));
+  revelarColorCarta($cartaClickeada, obtenerColorCarta($cartaClickeada.id));
 
-    if ($cartaGuardada) bloquearInput();
+  if ($cartaGuardada) bloquearInput();
 
-    setTimeout(() => {
-        if ($cartaGuardada === "") {
-            $cartaGuardada = $cartaClickeada;
-        } else {
-            manejarCartasElegidas($cartaClickeada);
-            chequearEstadoJuego();
-        }
+  setTimeout(() => {
+    if ($cartaGuardada === "") {
+      $cartaGuardada = $cartaClickeada;
+    } else {
+      manejarCartasElegidas($cartaClickeada);
+      chequearEstadoJuego();
+    }
 
-        desbloquearInput();
-    }, 300);
+    desbloquearInput();
+  }, 300);
 };
 
 const revelarColorCarta = ($carta, color) => {
-    $carta.style.backgroundColor = color;
+  $carta.style.backgroundColor = color;
 };
 
 const manejarCartasElegidas = ($cartaClickeada) => {
-    if ($cartaGuardada.id !== $cartaClickeada.id) {
-        const color$CartaGuardada = obtenerColorCarta($cartaGuardada.id);
-        const color$CartaClickeada = obtenerColorCarta($cartaClickeada.id);
+  const color$CartaGuardada = obtenerColorCarta($cartaGuardada.id);
+  const color$CartaClickeada = obtenerColorCarta($cartaClickeada.id);
 
-        if (color$CartaGuardada === color$CartaClickeada) {
-            $cartaGuardada.style.backgroundColor = "";
-            $cartaClickeada.style.backgroundColor = "";
-            pares--;
-        } else {
-            $cartaGuardada.style.backgroundColor = "brown";
-            $cartaClickeada.style.backgroundColor = "brown";
-        }
+  if (color$CartaGuardada === color$CartaClickeada) {
+    $cartaGuardada.style.backgroundColor = "";
+    $cartaClickeada.style.backgroundColor = "";
+    pares--;
+  } else {
+    $cartaGuardada.style.backgroundColor = "brown";
+    $cartaClickeada.style.backgroundColor = "brown";
+  }
 
-        $cartaGuardada = "";
-        actualizarIntentos();
-    }
+  $cartaGuardada = "";
+  actualizarIntentos();
 };
 
 const chequearEstadoJuego = () => {
-    if (pares === 0) {
-        clearInterval(intervalo);
-        actualizarAlerta(`Ganaste! Pulsa "Comenzar" para volver a jugar`);
-    }
+  if (pares === 0) {
+    clearInterval(intervalo);
+    actualizarAlerta(
+      `Ganaste! Intentos: ${intentos}, tiempo: ${tiempoJugando} segundos.`
+    );
+  }
 };
 
 const actualizarIntentos = () => {
-    intentos++;
-    $contador.textContent = intentos;
+  intentos++;
+  $contador.textContent = intentos;
 };
 
 const crearCarta = (id) => {
-    const $carta = document.createElement("div");
-    $carta.setAttribute("id", id);
-    $carta.className = "col carta";
-    $carta.style.backgroundColor = "brown";
+  const $carta = document.createElement("div");
+  $carta.setAttribute("id", id);
+  $carta.className = "col carta";
+  $carta.style.backgroundColor = "brown";
 
-    return $carta;
+  return $carta;
 };
 
 const obtenerColorCarta = (id) => {
-    const numeroId = Number(id.replace("carta-", ""));
-    return coloresRepetidos[numeroId];
+  const numeroId = Number(id.replace("carta-", ""));
+  return coloresRepetidos[numeroId];
 };
